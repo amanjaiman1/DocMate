4fca475d5ee1907fea690d3ae47abb3aac2a4fa0
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2024-01-14 16:30:14 +0530

    Navbar etc

---
+++ src/trpc/index.ts
@@ -0,0 +1,19 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { publicProcedure, router } from "./trpc";
+import { TRPCError } from "@trpc/server";
+
+export const appRouter = router({
+    authCallback: publicProcedure.query(() => {
+        const { getUser } = getKindeServerSession()
+        const user = getUser()
+        console.log(user)
+
+        if (!user.id || !user.email) {
+            throw new TRPCError({ code: 'UNAUTHORIZED' })
+        }
+
+        return { success: true}
+    })
+})
+
+export type AppRouter = typeof appRouter
